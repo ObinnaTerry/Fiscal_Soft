5783bc74cf07b3121584c7f7f0abdae69bb78067
youngjay90 <3254048915@qq.com>
2020-01-13 16:43:40 +0200

    Add POST data for HTTP request

--- heartbeat.py
+++ heartbeat.py
@@ -1,5 +1,38 @@
 import threading
 import time
+from _encrypt import DataEnc, key
+import json
+import requests
+
+
+encrypt = DataEnc()
+b_data = {"id": "531030026147", "lon": 100.832004, "lat": 45.832004, "sw_version": "1.2"}
+b_data_json = json.dumps(b_data)
+b_data_pad = encrypt.pad(b_data_json)
+b_data_des = encrypt.des_encrypt_64encode(b_data_pad)
+
+sign = encrypt.md5(b_data_des)
+key_ = encrypt.rsa_encrypt(key)
+
+HEADERS = {
+    'Content-Length': '1300',
+    'Content-Type': 'application/json',
+}
+
+request_data = {
+    "message": {
+        "body": {
+            "data": {
+                "device": "531030026147",
+                "serial": "000000",
+                "bus_id": "MONITOR-R",
+                "content": b_data_des.decode(),
+                "sign": sign.decode(),
+                "key": key_.decode()
+            }
+        }
+    }
+}
 
 
 class HeartBeat(threading.Thread):
@@ -15,9 +48,11 @@ class HeartBeat(threading.Thread):
         thread.start()
 
     def run(self):
-
         """ Method that runs in the background """
 
         while True:
             # more code to be inserted here
             time.sleep(self.interval)
+
+
+print(json.dumps(request_data))
