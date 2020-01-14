c9885d9b77adf36ef376dde4e9fc38b8746696bd
youngjay90 <3254048915@qq.com>
2020-01-14 14:45:37 +0200

    feat: Add decrytion
    Added DES and RSA decryption. DES decryption uses the 8-byte randon key
    returned by the server.

--- _encrypt.py
+++ _encrypt.py
@@ -15,7 +15,7 @@ class DataEnc:
     def __init__(self):
         self.cipher = DES.new(key, DES.MODE_ECB)
         self.hash = hashlib.md5()
-        with open('private_key.pem', mode='rb') as key_file:
+        with open('pp.pem', mode='rb') as key_file:
             self.private_key = serialization.load_pem_private_key(
                 key_file.read(),
                 password=None,
@@ -36,12 +36,28 @@ class DataEnc:
         data_encrypt = self.cipher.encrypt(data)
         return base64.b64encode(data_encrypt)
 
+    @staticmethod
+    def des_decrypt(d_key, message):
+        cipher = DES.new(d_key, DES.MODE_ECB)
+        message = base64.b64decode(message)
+        data_decrypt = cipher.decrypt(message)
+        data_decrypt_rm_pad = Padding.removeNullPadding(data_decrypt.decode(), 8)
+        return data_decrypt_rm_pad
+
     def rsa_encrypt(self, message):
         cipher_text = self.public_key.encrypt(
             message,
             padding.PKCS1v15()
         )
         return base64.b64encode(cipher_text)
+
+    def rsa_decrypt(self, message):
+        message = base64.b64decode(message)
+        plain_text = self.private_key.decrypt(
+            message,
+            padding.PKCS1v15()
+        )
+        return plain_text
 
     def md5(self, data):
         self.hash.update(data)
