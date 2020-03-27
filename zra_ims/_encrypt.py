4e4f8effa5967c77b1389abfaa1fcee5440e4b51
youngjay90 <3254048915@qq.com>
2020-03-27 11:37:40 +0200

    feat: Add read_db_config function to handle dB login creds

--- zra_ims/_encrypt.py
+++ zra_ims/_encrypt.py
@@ -1,9 +1,11 @@
-from Crypto.Cipher import DES
-# from secrets import token_bytes
-import Padding
+from configparser import ConfigParser
 import json
 import base64
 import hashlib
+# from secrets import token_bytes
+
+from Crypto.Cipher import DES
+import Padding
 from Crypto.Cipher import PKCS1_v1_5
 from Crypto.PublicKey import RSA
 from cryptography.hazmat.backends import default_backend
@@ -105,3 +107,25 @@ class DataEnc:
         data = self.pad(data)  # add padding
         data = self.des_encrypt_64encode(data)  # DES encrypt data
         return data.decode()  # encode data
+
+
+def read_db_config(filename='config.ini', section='mysql'):
+    """ Read database configuration file and return a dictionary object
+    :param filename: name of the configuration file
+    :param section: section of database configuration
+    :return: a dictionary of database parameters
+    """
+    # create parser and read ini configuration file
+    parser = ConfigParser()
+    parser.read(filename)
+
+    # get section, default to mysql
+    db_cred = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db_cred[item[0]] = item[1]
+    else:
+        raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+    return db_cred
